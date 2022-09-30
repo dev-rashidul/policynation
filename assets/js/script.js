15bd60cb2cec230615af98ef47c35725868460f4
dev-rashidul <rashidul.contact@gmail.com>
2022-09-30 16:37:41 +0600

    Website Complete

---
+++ assets/js/script.js
@@ -0,0 +1,65 @@
+// Testimonial Slider
+
+// Main Slider
+const galleryTop = new Swiper('.gallery-top', {
+	spaceBetween: 10,
+	loop: true,
+	loopedSlides: 4
+});
+
+// Pagination Slider
+const galleryThumbs = new Swiper('.gallery-thumbs', {
+	spaceBetween: 20,
+	centeredSlides: true,
+	slidesPerView: 5, // 'auto'
+	touchRatio: 0.2,
+	slideToClickedSlide: true,
+	loop: true,
+	loopedSlides: 4,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	pagination: {
+	el: ".swiper-pagination",
+	clickable: true,
+	},
+	breakpoints: {
+		300: {
+			slidesPerView: 2,
+		},
+		767: {
+			slidesPerView: 5,
+		}
+	},
+	
+});
+galleryTop.controller.control = galleryThumbs;
+galleryThumbs.controller.control = galleryTop;
+
+// Insurance Company Slider
+
+var swiper = new Swiper(".company-slider", {
+	spaceBetween: 20,
+	centeredSlides: true,
+	slidesPerView: 5, // 'auto'
+	touchRatio: 0.2,
+	slideToClickedSlide: true,
+	loop: true,
+	loopedSlides: 4,
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+	},
+	breakpoints: {
+		300: {
+			slidesPerView: 2,
+		},
+		767: {
+			slidesPerView: 5,
+		},
+		991: {
+			slidesPerView: 7,
+		}
+	},
+  });
